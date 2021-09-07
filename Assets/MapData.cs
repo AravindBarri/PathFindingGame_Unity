4feffbc702b05d17acbd3626d50bc6fb5e0dcec3
PTRL291\Aravind Barri <aravindbarri@gmail.com>
2021-09-07 16:54:49 +0530

    mapdata and nodes script added

---
+++ Assets/MapData.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapData : MonoBehaviour
+{
+    public int width = 10;
+    public int height = 5;
+
+    int[,] MakeMyMap()
+    {
+        int[,] map = new int[width, height];
+        for (int i = 0; i < width; i++)
+        {
+            for (int x = 0; x < height; x++)
+            {
+                map[i, x] = 0;
+                print(map[i, x]);
+
+            }
+
+        }
+        map[1, 0] = 1;
+        map[1, 1] = 1;
+        map[1, 2] = 1;
+        map[3, 2] = 1;
+        map[3, 3] = 1;
+        map[3, 4] = 1;
+        map[4, 2] = 1;
+        map[5, 1] = 1;
+
+
+        return map;
+    }
+    private void Start()
+    {
+        MakeMyMap();
+    }
+
+
+}
